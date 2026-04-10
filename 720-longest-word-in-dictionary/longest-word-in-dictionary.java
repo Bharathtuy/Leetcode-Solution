19a5937aa9fbbeaea7b0c0ec0e0cad30e1194ebd
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-10 16:01:02 +0530

    Time: 541 ms (5.07%) | Memory: 46.6 MB (67.30%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestWord(String[] words) {
+        List<String> list = new ArrayList<>();
+        Arrays.sort(words);
+        for( int i = 0 ;i < words.length ; i++)list.add(words[i]);
+        String ans ="";
+        for( int i = 0 ; i <words.length ;i++){
+            boolean flag = false;
+            for( int j = 0; j <words[i].length() ;j++){
+                if(list.contains(words[i].substring(0,j+1))){
+
+                }
+                else{
+                    flag = true;
+                    break;}
+            }
+            if(flag==false){
+                if(ans.length()< words[i].length()){
+                    ans=words[i];
+                }
+            }
+
+
+        }
+        return ans;
+    }
+}
