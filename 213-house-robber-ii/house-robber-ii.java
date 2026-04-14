21ad2d17778d585f3b35de825e513882e76db3b4
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-14 18:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (91.04%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,26 +1,24 @@
 class Solution {
-    // int dp[];
-    public int robb(int []nums,int i,int dp[]){
-      if(i==0)return nums[0];
-      if( i< 0) return 0;
-      if(dp[i]!=-1)return dp[i];
-      int c = nums[i]+robb(nums,i-2,dp);
-      int nc = 0 + robb(nums ,i-1,dp);
-      return dp[i]=Math.max(c,nc);
-
+    public int find(int n[] , int dp[] , int i){
+        if( i == 1  )return n[1];
+        if ( i < 1 )return 0 ;
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=Math.max(n[i]+find(n,dp,i-2),find(n,dp,i-1));
     }
-    public int rob(int[] nums) {
-        if(nums.length==1)
-        return nums[0];
-       int  dp[] = new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        int ans =  robb(nums,nums.length-2,dp);
-        int nums2[] = new int [nums.length-1];
-        for( int  i = 1; i< nums.length; i++)
-        nums2[i-1]=nums[i];
-        Arrays.fill(dp,-1);
-        int ans2 =robb(nums2,nums2.length-1,dp);
-        return Math.max(ans,ans2);
+    public int find2(int n[] , int dp[] , int i){
+        if( i == 0  )return n[0];
+        if ( i < 0 )return 0 ;
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=Math.max(n[i]+find2(n,dp,i-2),find2(n,dp,i-1));
+    }
 
+    public int rob(int[] nums) {
+        if( nums.length == 1)return nums[0];
+        int[]dp = new int [nums.length];
+        Arrays.fill(dp,-1);
+        int[]dp2 = new int [nums.length];
+        Arrays.fill(dp2,-1);
+        return Math.max(find(nums,dp,nums.length-1),find2(nums,dp2,nums.length-2));
+        
     }
 }
