a1e772c113891d01809bfa93f3fcd843dc33aeba
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-15 12:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (72.75%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        if(matrix.length==1 || matrix[0].length==1)return true;
-        for( int i = 0 ; i < matrix[0].length;i++){
-            int j = 0 ;
+        if (matrix.length == 1 || matrix[0].length == 1)
+            return true;
+        for (int i = 0; i < matrix[0].length; i++) {
+            int j = 0;
             int t = matrix[j][i];
-            while(i+j < matrix[0].length && j < matrix.length){
-                System.out.println(matrix[j][i]+"");
-                if(matrix[j][i+j]!=t )return false;
+            while (i + j < matrix[0].length && j < matrix.length) {
+                if (matrix[j][i + j] != t)
+                    return false;
                 j++;
-                // i++;
-
             }
         }
-        for( int i = 1 ; i < matrix.length ; i++){
-             int j = 0;
-             int t = matrix[i][j];
-             while( i+j < matrix.length && j < matrix[0].length ){
-                if(matrix[i+j][j]!=t)return false;
+        for (int i = 1; i < matrix.length; i++) {
+            int j = 0;
+            int t = matrix[i][j];
+            while (i + j < matrix.length && j < matrix[0].length) {
+                if (matrix[i + j][j] != t)
+                    return false;
                 j++;
-             } 
+            }
         }
         return true;
-        
     }
 }
