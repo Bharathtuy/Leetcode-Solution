7738e378eb6baf6ab756cc7b33c73a15156bdf2e
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-27 10:52:04 +0530

    Time: 24 ms (7.25%) | Memory: 44.7 MB (12.64%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -1,8 +1,13 @@
 class Solution {
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
         List<String> ans =  new ArrayList<>();
-
+        HashSet<String> set = new HashSet<>();
+        for( int i = 0 ; i < dictionary.length ; i++)set.add(dictionary[i]);
         for ( int i = 0 ; i < queries.length ; i++){
+            if ( set.contains(queries[i])){
+                ans.add(queries[i]);
+                continue;
+            }
             for ( int j = 0 ; j < dictionary.length ;j++){
                 int limit = 2 ;
                 int f = 0 ;
