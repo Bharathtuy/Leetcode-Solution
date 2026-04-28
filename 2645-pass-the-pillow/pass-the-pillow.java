f471629075b8f78a360b4286ae2ee8f4a541ecfe
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-28 14:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (18.79%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int c = time / (n-1 );
+        int r = time % (n-1) ;
+        if (c % 2 == 0){
+            return r+1;
+        }
+        else {
+            if ( r== 0)return n;
+        return n-r;}
+    }
+}
