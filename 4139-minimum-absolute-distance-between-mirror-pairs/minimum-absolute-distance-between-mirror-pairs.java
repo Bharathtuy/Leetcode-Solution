39b02fd1aa54e9e69caada584b5d1b7672b76609
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-04-29 10:50:42 +0530

    Time: 116 ms (7.02%) | Memory: 125.6 MB (5.01%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -1,9 +1,8 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        int ans = Integer.MAX_VALUE;
+     int ans = Integer.MAX_VALUE;
      HashMap<Integer,List<Integer>> map = new HashMap<>();
      for( int i = 0 ; i < nums.length ; i++){
-        
         if ( map.containsKey(nums[i]))
         map.get(nums[i]).add(i);
         else{
@@ -11,22 +10,19 @@ class Solution {
             k.add(i);
             map.put(nums[i],k);
         }
-
      }
-    //  System.out.println(map);
      for( int i = 0 ; i < nums.length ; i++){
         String t = Integer.toString(nums[i]);
         String reversed = new StringBuilder(t).reverse().toString();
         int k = Integer.parseInt(reversed);
-        if ( map.containsKey(k)  ){
+        if ( map.containsKey(k)){
             List<Integer> l = map.get(k);
             for ( int j = 0; j < l.size() ;j++){
                 if ( l.get(j)!=0 && l.get(j)>i){
                      ans = Math.min(ans , Math.abs(i-l.get(j)));
-                     
+                if ( ans == 1)return 1;
                 }
             }
-           
         }
         if ( ans == 1)return 1;
      }
