05f5f53685b88169268e69236aeb7989f5cad52d
Bharathtuy <130954671+Bharathtuy@users.noreply.github.com>
2026-05-14 11:05:31 +0530

    Time: 6 ms (15.24%) | Memory: 44.6 MB (70.86%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -3,15 +3,18 @@ class Solution {
         int cnt = 0;
         int max = 0;
         Arrays.sort(nums);
-        max = nums[nums.length-1];
-        if(max == nums[0])cnt++;
-        for( int i = 1 ; i < nums.length ; i++){
-            if(nums[i]==nums[i-1] && nums[i]!=max){
+        max = nums[nums.length - 1];
+        if (max == nums[0])
+            cnt++;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] && nums[i] != max) {
                 return false;
             }
-            if ( nums[i] == max) cnt++;
+            if (nums[i] == max)
+                cnt++;
         }
-        if(cnt == 2 && max+1== nums.length)return true;
+        if (cnt == 2 && max + 1 == nums.length)
+            return true;
         return false;
     }
 }
